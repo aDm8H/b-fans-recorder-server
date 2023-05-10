71b9f55ca36bf654700e53c5c022e5bc989a6805
aDm8H <xiaoxuan010n@gmail.com>
2023-05-10 18:06:02 +0800

    v1.1.0: 重构主程序，引入better-sqlite3

--- README.md
+++ README.md
@@ -6,12 +6,12 @@
 
 #### 方式一：使用可执行文件 exe（仅限 Windows 系统）
 
-1. 在[release](https://github.com/aDm8H/b-fans-recorder-server/releases)处下载`b-fans-recorder-server.exe`主程序。
-2. 启动`b-fans-recorder-server.exe`服务器，启动后将自动弹出前端网页。
+1. 在[release](https://github.com/aDm8H/b-fans-recorder-server/releases)处下载`b-fans-recorder-server.zip`压缩包，并解压。
+2. 双击启动`b-fans-recorder-server.exe`服务器，启动后将自动弹出前端网页。
 
 #### 方式二：使用 JavaScript 源码运行
 
-1. 请先安装好[NodeJs](https://nodejs.org/)环境，建议版本18以上。
+1. 请先安装好[NodeJs](https://nodejs.org/)环境，建议版本 18 以上。
 
 2. 安装好相关依赖：
 
@@ -27,6 +27,6 @@
 >
 > `pm2 start main.js`
 
-### 使用本地前端
+### 使用本地前端（推荐）
 
 默认情况下，前端页面从 github.io 加载，加载速度可能受限于网络。可前往下载[B 站粉丝记录前端显示界面](https://github.com/aDm8H/b-fans-record-front/)，按照相关提示在电脑本地开启网页。
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const sqlite3 = require("sqlite3");
+const Database = require("better-sqlite3");
 const child_process = require("child_process");
 const WebSocket = require("ws");
 const fs = require("fs");
-
+const path = require("path");
 
 const DEFAULT_CONFIG = {
     targetUID: "17603188",
@@ -21,23 +21,18 @@ try {
     fs.writeFileSync("global-config.json", JSON.stringify(DEFAULT_CONFIG));
     GLOBAL_CONFIG = DEFAULT_CONFIG;
 }
-const apiURI = "http://api.bilibili.com/x/relation/stat?vmid=";
+const B_FANS_API_PREFIX = "http://api.bilibili.com/x/relation/stat?vmid=";
 
 //数据库初始化
-var db;
-db = new sqlite3.Database(`${GLOBAL_CONFIG.targetUID}.db`, function (e) {
-    if (e) throw e;
-    console.log("[数据库]成功打开db文件.");
-});
-
-db.run("CREATE TABLE fans(datetime INTEGER,fans INTEGER);", e => {
-    if (e) {
-        if (e.errno == 1) console.log("[数据库]数据表已创建过.");
-        else throw e;
-    } else {
-        console.log("[数据库]成功建表.");
-    }
-});
+try {
+    var dbFilePath = path.join(process.cwd(), GLOBAL_CONFIG.targetUID + ".db");
+    var db = new Database(dbFilePath);
+    const initTableSQL = "CREATE TABLE IF NOT EXISTS fans(datetime INTEGER,fans INTEGER)";
+    db.exec(initTableSQL);
+} catch (err) {
+    console.error("[数据库初始化]失败: ", err, "[程序退出]");
+    process.exit(1);
+}
 
 //WebSocket服务器初始化
 const WebSocketServer = WebSocket.Server;
@@ -64,59 +59,83 @@ server.on("connection", ws => {
             req = JSON.parse(reqStr);
         } catch (error) {
             console.warn("[WS]请求的JSON解析失败.", error);
+            return;
         }
+        var reqVer = req.verion;
+        if (typeof req.verion == "undefined" || reqVer == 1) {
+            handleV1(req);
+        }
+    });
+
+    function handleV1(req) {
+        const rtStmt = db.prepare("SELECT * FROM fans ORDER BY datetime DESC LIMIT 1"),
+            allDataStmtNoSampling = db.prepare("SELECT * FROM fans ORDER BY datetime DESC"),
+            allDataStmtSampling = db.prepare(
+                "SELECT * FROM (SELECT * FROM(SELECT ROW_NUMBER() OVER (ORDER BY datetime DESC) AS rn, * from fans) ) WHERE rn%60=1"
+            ),
+            rangeDataStmtNoSampling = db.prepare("SELECT * FROM fans ORDER BY datetime DESC LIMIT @range"),
+            rangeDataStmtSampling = db.prepare(
+                "SELECT * FROM (SELECT * FROM(SELECT ROW_NUMBER() OVER (ORDER BY datetime DESC) AS rn, * FROM fans) LIMIT @range ) WHERE rn%60=1"
+            );
 
         if (req.type == "rt") {
-            db.get("SELECT * FROM fans ORDER BY datetime DESC LIMIT 1", (err, dbResData) => {
-                if (err) {
-                    console.error("[数据库]读取最新数据时出错.");
-                    return;
-                }
-                dbResArray = [dbResData.datetime, dbResData.fans];
-                ws.send(JSON.stringify({ type: "rt", data: dbResArray }));
-                console.debug("[WS]已发送最新一行数据.");
-            });
+            try {
+                var dbResData = rtStmt.get();
+            } catch (err) {
+                console.error("[数据库]读取最新数据时出错.");
+                return;
+            }
+            dbResArray = [dbResData.datetime, dbResData.fans];
+            ws.send(JSON.stringify({ type: "rt", data: dbResArray }));
+            console.debug("[WS]已发送最新一行数据.");
         } else if (req.type == "all") {
-            if (typeof req.sampling == "undefined" || !req.sampling) var sql = "SELECT * FROM fans ORDER BY datetime DESC";
-            else var sql = "SELECT * FROM (SELECT * FROM(SELECT ROW_NUMBER() OVER (ORDER BY datetime DESC) AS rn, * from fans) ) WHERE rn%60=1";
-            db.all(sql, (err, dbResData) => {
-                if (err) {
-                    console.error("[数据库]读取全部数据时出错. 降采样策略为: ", req.sampling);
-                    return;
-                }
-                dbResArray = dbResData.map(row => [row.datetime, row.fans]);
-                ws.send(JSON.stringify({ type: "all", data: dbResArray }));
-                console.debug("[WS]已发送全部数据.降采样策略为: ", req.sampling);
-            });
+            var dbResData;
+            try {
+                if (typeof req.sampling == "undefined" || !req.sampling) dbResData = allDataStmtNoSampling.all();
+                else dbResData = allDataStmtSampling.all();
+            } catch (err) {
+                console.error("[数据库]读取全部数据时出错. 降采样策略为: ", req.sampling);
+                return;
+            }
+
+            dbResArray = dbResData.map(row => [row.datetime, row.fans]);
+            ws.send(JSON.stringify({ type: "all", data: dbResArray }));
+            console.debug("[WS]已发送全部数据.降采样策略为: ", req.sampling);
         } else if (req.type == "range") {
             var len = req.len;
             if (!len) len = 12;
-            if (typeof req.sampling == "undefined" || !req.sampling) var sql = "SELECT * FROM fans ORDER BY datetime DESC LIMIT ?";
-            else var sql = "SELECT * FROM (SELECT * FROM(SELECT ROW_NUMBER() OVER (ORDER BY datetime DESC) AS rn, * FROM fans) LIMIT ? ) WHERE rn%60=1";
-            db.all(sql, len, (err, dbResData) => {
-                if (err) {
-                    console.error("[数据库]读取区间数据时出错. 区间长度为", len, "降采样策略为: ", req.sampling);
-                    return;
+            try {
+                var dbResData;
+                if (typeof req.sampling == "undefined" || !req.sampling) dbResData = rangeDataStmtNoSampling.all({ range: len });
+                else {
+                    dbResData = rangeDataStmtSampling.all({ range: len });
                 }
-                dbResArray = dbResData.map(row => [row.datetime, row.fans]);
-                ws.send(JSON.stringify({ type: "range", len: len, data: dbResArray }));
-                console.debug("[WS]已发送区间数据. 长度为", len, "降采样策略为: ", req.sampling);
-            });
+            } catch (err) {
+                console.error("[数据库]读取区间数据时出错. 区间长度为", len, "降采样策略为: ", req.sampling);
+                return;
+            }
+
+            dbResArray = dbResData.map(row => [row.datetime, row.fans]);
+            ws.send(JSON.stringify({ type: "range", len: len, data: dbResArray }));
+            console.debug("[WS]已发送区间数据. 长度为", len, "降采样策略为: ", req.sampling);
         }
-    });
+    }
 });
 
-console.log("[主程序]开始进行记录.");
+console.log("[主进程]开始进行记录.");
 recordOnce();
-setInterval(recordOnce, GLOBAL_CONFIG.requestFansInterval);
+setTimeout(() => {
+    recordOnce();
+    setInterval(recordOnce, GLOBAL_CONFIG.requestFansInterval);
+}, 5000 - (new Date().getTime() % 5000));
 function recordOnce() {
-    var uri = apiURI + GLOBAL_CONFIG.targetUID;
+    var uri = B_FANS_API_PREFIX + GLOBAL_CONFIG.targetUID;
+    const recordStmt = db.prepare("INSERT INTO fans VALUES ( @time , @num )");
     fetch(uri)
         .then(apiRes => {
             // console.debug(apiRes);
             if (apiRes.status != 200) {
                 throw new Error("[Get-API]HTTP状态码异常", apiRes);
-                return;
             }
             return apiRes.json();
         })
@@ -124,7 +143,7 @@ function recordOnce() {
             var dt = new Date();
             var fans = data.data.follower;
             // console.log(moment(dt).format(), "实时粉丝数:", fans);
-            db.run("INSERT INTO fans VALUES (?,?)", dt.getTime(), fans);
+            recordStmt.run({ time: dt.getTime(), num: fans });
             return [dt.getTime(), fans];
         })
         .then(row => {
@@ -133,11 +152,10 @@ function recordOnce() {
                 client.send(JSON.stringify({ type: "rt", data: row }));
             });
         })
-        .catch(e => {
-            console.error(e);
+        .catch(err => {
+            console.warn("[Get-API]Fetch失败! ");
         });
 }
-
 //打开浏览器
 
 child_process.exec(`start https://adm8h.github.io/b-fans-record-front/?ws=ws://127.0.0.1:${GLOBAL_CONFIG.websocketServerPort}`);
